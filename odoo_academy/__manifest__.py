a6b8dd668854a2bfa80687507911908659c09123
yazh-odoo <yazh@odoo.com>
2022-06-16 11:19:38 -0400

    [ADD] sale.form inheritance

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -2,34 +2,35 @@
 
 {
     'name': 'Odoo Academy',
-    
+
     'summary': """Academy app to manage Training""",
-    
+
     'description': """
         Academy Module to manage Training:
         - Courses
         - Sessions
         - Attendees
     """,
-    
+
     'author': 'Odoo',
-    
+
     'website': 'https://www.odoo.com',
-    
+
     'category': 'Training',
-    
+
     'version': '0.1',
-    
-    'depends': ['base'],
-    
+
+    'depends': ['sale'],
+
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
         "views/session_views.xml",
+        "views/sale_views_inherit.xml",
     ],
-    
+
     'demo': [
         'demo/academy_demo.xml',
     ],
