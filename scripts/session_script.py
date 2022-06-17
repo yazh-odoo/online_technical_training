da280a251c5d38c5901d5dba2fd5bbc15f638d5e
yazh-odoo <yazh@odoo.com>
2022-06-17 11:26:55 -0400

    [ADD] new scripts

---
+++ scripts/session_script.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from xmlrpc import client
+
+url = 'https://yazh-odoo-online-technical-training-dev-5165634.dev.odoo.com/'
+db = 'yazh-odoo-online-technical-training-dev-5165634'
+username = 'admin'
+password = 'admin'
+
+common = client.ServerProxy("{}/xmlrpc/2/common".format(url))
+print(common.version())
+
+uid = common.authenticate(db, username, password, {})
+print(uid)
+
+models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
+
+model_access = models.execute_kw(db, uid, password,
+                                 'academy.session', 'check_access_rights',
+                                 ['write'], {'raise_exception': False})
+print(model_access)
+
+courses = models.execute_kw(db, uid, password,
+                            'academy.course', 'search_read',
+                            [[['level', 'in', ['intermediate', 'beginner']]]])
+print(courses)
+
+course = models.execute_kw(db, uid, password,
+                           'academy.course', 'search',
+                           [[['name', '=', 'Accounting 200']]])
+print(course)
+
+session_fields = models.execute_kw(db, uid, password,
+                                   'academy.session', 'fields_get',
+                                   [], {'attributes': ['string', 'type', 'required']})
+print(session_fields)
+
+new_session = models.execute(db, uid, password,
+                             'academy.session', 'create',
+                             [
+                                 {
+                                     'course_id': course[0],
+                                     'state': 'open',
+                                     'duration': 5,
+                                 }
+                             ])
+print(new_session)
